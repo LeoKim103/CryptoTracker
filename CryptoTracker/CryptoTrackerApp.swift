25d3330024d189017922ef66a9664f9432d1a5bf
Leo Kim <leokim1032209@gmail.com>
2021-10-18 14:55:01 +0700

    initial commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Leo Kim on 18/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
